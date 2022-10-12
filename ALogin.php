6c1b6b26551b745183990d891fbc45c2266da2a9
Ayman <89187064+AymanTTV@users.noreply.github.com>
2022-10-12 08:19:30 +0300

    Login
    MiigSolution

---
+++ ALogin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="Alogin.css
+	">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		Login
+	</title>
+</head>
+<body>
+<div class="container"></div>
+</body>
+</html>
+
+<?php 
+include "style.html";
+
+ ?>
+
